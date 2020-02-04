b3389b58bb352e37ddc0b77fd754fdebd72ca774
Chaim <calvinfriedman@Calvins-MacBook-Pro-2.local>
2020-02-03 23:22:32 -0500

    starter files

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.listen(1337, () => console.log("server is running on leet"));
